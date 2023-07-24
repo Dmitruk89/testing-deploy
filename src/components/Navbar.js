de7ac9a261afc14b154780bb59c543bef121ec8a
Sergey Lebed <swansergey7@gmail.com>
2023-07-24 11:44:27 +0300

    accessing context with useContect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,25 @@
 import "./Navbar.css";
 import { Link } from "react-router-dom";
-
 import React from "react";
 import Searchbar from "./Searchbar";
+import { useContext } from 'react'
+import { ThemeContext } from '../context/ThemeContext'
+
 
 function Navbar() {
+	const { color } = useContext(ThemeContext)
+
+
   return (
-    <div className="navbar">
-      <nav>
-        <Link to="/" className="brand">
-          <h1>Cooking Recipes</h1>
-        </Link>
-		  <Searchbar />
-        <Link to='/create'>Create Recipe</Link>
-      </nav>
-    </div>
+	<div className="navbar" style={{ background: color }}>
+	<nav>
+	  <Link to="/" className="brand">
+		 <h1>Cooking Recipes</h1>
+	  </Link>
+	  <Searchbar />
+	  <Link to="/create">Create Recipe</Link>
+	</nav>
+ </div>
   );
 }
 
