42bd7be5d65236e215be6bbb68835338ab6262b5
Sergey Lebed <swansergey7@gmail.com>
2023-07-27 08:26:18 +0300

    Making a Light & Dark Mode

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -1,27 +1,38 @@
 import './ThemeSelector.css';
 import React from 'react'
-import {useTheme} from '../hooks/useTheme'
+
+import modeIcon from '../assets/mode-icon.svg'
+import { useTheme } from "../hooks/useTheme"
 
 	const themeColors = ['#420420', '#003366', '#bada55']
 
-
-function ThemeSelector() {
-	const { changeColor } = useTheme()
-
-
-  return (
-	 <div className='theme-selector'>
-		<div className="theme-buttons">
-			{themeColors.map(color => (
-				<div 
-					key={color}
-					onClick={() => changeColor(color)}
-					style={{background: color}}
+	export default function ThemeSelector() {
+		const { changeColor, changeMode, mode } = useTheme()
+	 
+		const toggleMode = () => {
+			changeMode(mode === 'dark' ? 'light' : 'dark')
+		}
+		console.log(mode)
+	 
+		return (
+		  <div className="theme-selector">
+			 <div className="mode-toggle">
+				<img 
+				  onClick={toggleMode}
+				  src={modeIcon}
+				  style={{ filter: mode === 'dark' ? 'invert(100%)' : 'invert(20%)' }}
+				  alt="dark/light toggle icon"
 				/>
-			))}
-		</div>
-	 </div>
-  )
-}
-
-export default ThemeSelector
+			 </div>
+			 <div className="theme-buttons">
+				{themeColors.map(color => (
+				  <div 
+					 key={color} 
+					 onClick={() => changeColor(color)}
+					 style={{ background: color }}
+				  />
+				))}
+			 </div>
+		  </div>
+		)
+	 }
